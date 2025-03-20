392413bde84b5114cad974933431f9263fe7cdf3
Timothy Feng <timothyfeng19@gmail.com>
2025-03-20 12:16:40 -0700

    Update cube.py

--- cube.py
+++ cube.py
@@ -1,13 +1,29 @@
 import turtle
 import math
 
-zoom = 7
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.penup()
+
 rotation_x = 0
 rotation_y = 0
+zoom = 7
 
 def coords (x, y, z):
-  return (cylind(math.atan2(int(y), int(x)), math.sqrt(int(x)^2+int(y)^2), z))
-  
+  return cylind(math.atan2(y, x), math.sqrt(x^2+y^2), z)
+    
 def cylind (deg, r, h):
-  return ((int(zoom)/(int(zoom)-int(z(deg, r, h))))*int(r)*math.cos(int(rotation_y)+int(deg)),(int(zoom)/(int(zoom)-int(z(deg, r, h))))*(int(r)*math.sin(int(rotation_y)+int(deg))+int(h)*math.cos(int(rotation_x))))
+  global rotation_x
+  global rotation_y
   
+  if (zoom-int(zz(deg, r, h)) > 0):
+    return (zoom/(zoom-int(findz(deg, r, h))))*r*math.cos(rotation_y+deg), (zoom/(zoom-int(findz(deg, r,h))))*(r*math.sin(rotation_x)*math.sin(rotation_y + deg)+h*math.cos(rotation_x))
+  else:   
+    return false
+        
+def findz(deg, r, h):
+  global rotation_x
+  global rotation_y
+    
+  return math.cos(rotation_x)*r*math.sin(rotation_y+deg)-math.sin(rotation_x)*h
+
