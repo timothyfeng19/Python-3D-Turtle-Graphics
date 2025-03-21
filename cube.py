29726ca4c134d403838c42ba0ed71fbaf1049ad0
Timothy Feng <timothyfeng19@gmail.com>
2025-03-20 22:03:10 -0700

    Update cube.py
    created one cube face, needs other 5 and also a way to change rotation

--- cube.py
+++ cube.py
@@ -1,27 +1,27 @@
-import turtle as cube
+import turtle
 import math
 
-cube.title = "3D Cube using Python Turtle"
-cube.setup(width=1000, height=1000)
+screen = turtle.Screen()
+screen.title = "3D Cube using Python Turtle"
+screen.setup(width=1000, height=1000)
+screen.tracer(0)
+
+cube = turtle.Turtle()
 cube.hideturtle()
 cube.penup()
-cube.tracer(0)
 
 rotation_x = 0
 rotation_y = 0
 zoom = 7
 
 def coords (x, y, z):
-  return cylind(math.atan2(y, x), math.sqrt(x^2+y^2), z)
+  return cylind(math.atan2(y, x), math.sqrt(x*x+y*y), z)
     
 def cylind (deg, r, h):
   global rotation_x
   global rotation_y
   
-  if (zoom-int(findz(deg, r, h)) > 0):
-    return (zoom/(zoom-int(findz(deg, r, h))))*r*math.cos(rotation_y+deg), (zoom/(zoom-int(findz(deg, r,h))))*(r*math.sin(rotation_x)*math.sin(rotation_y + deg)+h*math.cos(rotation_x))
-  else:   
-    return False
+  return ((zoom/(zoom-int(findz(deg, r, h))))*r*math.cos(rotation_y+deg))*100, ((zoom/(zoom-int(findz(deg, r,h))))*(r*math.sin(rotation_x)*math.sin(rotation_y + deg)+h*math.cos(rotation_x)))*100
         
 def findz(deg, r, h):
   global rotation_x
@@ -30,5 +30,12 @@ def findz(deg, r, h):
   return math.cos(rotation_x)*r*math.sin(rotation_y+deg)-math.sin(rotation_x)*h
 
 while (True):
-  cube.update()
-  cube.exitonclick()
+  cube.goto(coords(1, 1, 1))
+  cube.pendown()
+  cube.goto(coords(1, -1, 1))
+  cube.goto(coords(-1, -1, 1))
+  cube.goto(coords(-1, 1, 1))
+  cube.goto(coords(1, 1, 1))
+  cube.penup()
+
+  turtle.mainloop()
